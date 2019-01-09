021fe0e77ec94044b2f1fbbdac0c55a232fed4fd
Estevan Cruz <Estevancruz@Estevans-MBP.lan>
2019-01-09 12:55:36 -0600

    Question 1 tested and logged

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -3,3 +3,11 @@
 // change screen to reveal correct answer questions, incorrect, # of unanswered
 // timer that counts down as you start the game; closes game when time is up
 
+//setting up quetions in an array of objects/questions with an answer key
+var questions = [
+    { prompt: "What year did the Chicago Fire take place?"
+    
+    }
+    
+];
+console.log(questions)
