52ee08e4dfc3a6f898bf513f38ab5f6d709cba3f
Estevan Cruz <Estevancruz@Estevans-MBP.lan>
2019-01-09 13:44:47 -0600

    Question 2 finished and works in console

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -4,7 +4,7 @@
 // timer that counts down as you start the game; closes game when time is up
 
 //setting up quetions in an array of objects/questions with an answer key
-var questions = 
+var questions = [
     {
     prompt: "What year did the Chicago Fire take place?",
     options:{
@@ -14,5 +14,16 @@ var questions =
         d: "1920"
     },
     answer: "b"
-    };
+    },
+    {
+    prompt: "What does LSD Stand for?",
+    options:{
+        a: "Lysergic acid diethylamide",
+        b: "Laugh Smile and Dance",
+        c: "Long Sugar Donut",
+        d: "Lake Shore Drive"
+    },
+    answer: "d"
+    }
+]
 console.log(questions)
